70d41aae571ed121f526d64a845a002a1d5293c0
Шаймухаметов Ильдар <shaimildar@gmail.com>
2022-10-09 12:23:12 +0500

    random joke

--- app/routes/jokes/$id.tsx
+++ app/routes/jokes/$id.tsx
@@ -1,11 +1,31 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+import { db } from "~/utils/db.server";
+import type { Joke } from "@prisma/client";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+
+  return json(joke);
+};
+
 export default function JokeRoute() {
+  const joke = useLoaderData<Joke>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name} Permalink</Link>
     </div>
   );
 }
